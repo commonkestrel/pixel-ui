7aa9ddf250f595f86bc50fb4d3e4021081660ae7
commonkestrel <jettb6@gmail.com>
2024-06-07 23:09:20 -0700

    Add a `prelude`

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
 pub mod draw;
 pub mod element;
 pub mod event;
-pub mod signal;
+pub mod react;
 pub mod util;
-pub mod window;
+pub mod app;
+
+pub mod prelude {
+    pub use super::app::{Application, ApplicationBuilder};
+    pub use super::react::{WriteSignal, ReadSignal};
+    pub use super::event::*;
+    pub use super::element::{
+        canvas::Canvas,
+        text::Text,
+        icon::Icon,
+        button::Button,
+    };
+}
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
